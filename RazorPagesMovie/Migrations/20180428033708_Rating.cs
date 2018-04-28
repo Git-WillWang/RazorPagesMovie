51de542f7c3f70298715cd3522d88557840c1d77
Git-WillWang <wyjwillsmith@gmail.com>
2018-04-28 16:03:14 +0800

    添加项目文件。

---
+++ RazorPagesMovie/Migrations/20180428033708_Rating.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Title",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Genre",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
